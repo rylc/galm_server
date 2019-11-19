d6d579753483448f97b9b1f0ff8a0de92c6c86de
rylc <rylcii@163.com>
2019-11-19 21:25:33 +0800

    init

---
+++ src/main/java/com/brightms/galgame/GalgameApplication.java
@@ -0,0 +1,13 @@
+package com.brightms.galgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brightms/galgame/GalgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brightms.galgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
